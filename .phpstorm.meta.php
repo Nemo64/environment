bb6cdc100a1df1efb7812a7c384d5f4f2496f0db
Marco Pfeiffer <git@marco.zone>
2018-04-03 11:13:33 +0200

    more mock objects

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,4 +3,5 @@
 namespace PHPSTORM_META;
 
 override(\PHPUnit\Framework\TestCase::createMock(), type(0));
+override(\PHPUnit\Framework\TestCase::createConfiguredMock(), type(0));
 override(\PHPUnit\Framework\TestCase::createTestProxy(), type(0));
