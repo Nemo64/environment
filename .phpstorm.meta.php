be9b34bfb57908677b92f118f710e7c232f16933
Marco Pfeiffer <git@marco.zone>
2018-04-04 09:59:15 +0200

    add container to metadata

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,3 +5,4 @@ namespace PHPSTORM_META;
 override(\PHPUnit\Framework\TestCase::createMock(), type(0));
 override(\PHPUnit\Framework\TestCase::createConfiguredMock(), type(0));
 override(\PHPUnit\Framework\TestCase::createTestProxy(), type(0));
+override(\Nemo64\Environment\ConfiguratorContainer::get(), type(0));
