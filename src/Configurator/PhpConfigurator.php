c667adbc248c4ab60f6d6d7bbcd35da5447af1ca
Marco Pfeiffer <git@marco.zone>
2018-05-05 17:09:27 +0200

    get the php version from the platform hint

--- src/Configurator/PhpConfigurator.php
+++ src/Configurator/PhpConfigurator.php
@@ -3,6 +3,9 @@
 namespace Nemo64\Environment\Configurator;
 
 
+use Composer\Config\JsonConfigSource;
+use Composer\Json\JsonFile;
+use Composer\Package\Version\VersionParser;
 use Nemo64\Environment\ConfiguratorContainer;
 use Nemo64\Environment\ExecutionContext;
 
@@ -17,9 +20,25 @@ class PhpConfigurator implements ConfiguratorInterface
 
     public function configure(ExecutionContext $context, ConfiguratorContainer $container): void
     {
+        $versionParser = new VersionParser();
+        $platform = $context->getComposer()->getConfig()->get('platform');
+        if (!isset($platform['php'])) {
+            $phpVersion = $versionParser->normalize(PHP_VERSION);
+            $question = "What version of php do you want to use? (default $phpVersion): ";
+            $platform['php'] = $context->getIo()->ask($question, $phpVersion);
+            $context->getComposer()->getConfig()->merge(['platform' => $platform]);
+
+            $composerFilePath = trim(getenv('COMPOSER')) ?: $context->getPath('composer.json');
+            $composerFile = new JsonFile($composerFilePath, null, $context->getIo());
+            $configSource = new JsonConfigSource($composerFile);
+            $configSource->addConfigSetting('platform', $platform);
+        }
+
+        $version = implode('.', array_slice(explode('.', $versionParser->normalize($platform['php'])), 0, 2));
+
         $docker = $container->get(DockerConfigurator::class);
         $docker->createDockerfile('Dockerfile-php', [
-            'FROM chialab/php:7.1-apache',
+            "FROM chialab/php:$version-apache",
             'RUN ' . implode(" \\\n && ", [
                 "sed -i 's#/var/www/html#/var/www/public#g' /etc/apache2/sites-enabled/000-default.conf",
                 "a2enmod alias deflate expires headers rewrite",
