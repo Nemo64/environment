2ea557058baaf2fa98d79ca8bbf81d24a8f5f170
Marco Pfeiffer <git@marco.zone>
2018-05-16 18:53:44 +0200

    less noice around log target

--- src/Configurator/DockerConfigurator.php
+++ src/Configurator/DockerConfigurator.php
@@ -152,9 +152,8 @@ class DockerConfigurator implements ConfiguratorInterface
 
             $make['.PHONY']->addDependency($make['log']);
             $make['log']->setDescription("Show (and follow) the log files.");
-            $make['log']->setEnvironment('FOLLOW', 1);
-            $make['log']->setEnvironment('LINES', 20);
-            $make['log']->addCommand('docker-compose logs --tail=$(LINES) $(if $(filter 1,$(FOLLOW)),--follow)');
+            $make['log']->setEnvironment('LOG_PARAMS', '--tail=20 --follow');
+            $make['log']->addCommand('docker-compose logs $(LOG_PARAMS)');
         }
     }
 }
